c04849771b658c0f3807c9fe19dbb9db1c029961
qqibrow <qqibrow@gmail.com>
2014-09-28 13:58:35 -0700

    clang format file.

--- src/Sequence.cc
+++ src/Sequence.cc
@@ -5,17 +5,13 @@ using namespace qqibrow::Disruptor;
 
 const long Sequence::INITIAL_VALUE = -1;
 
-Sequence::Sequence() : Sequence(INITIAL_VALUE){
-
-}
+Sequence::Sequence() : Sequence(INITIAL_VALUE) {}
 
 Sequence::Sequence(long value) {
     this->value_.store(value, memory_order_relaxed);
 }
 
-long Sequence::Get() const {
-    return value_.load(memory_order_relaxed);
-}
+long Sequence::Get() const { return value_.load(memory_order_relaxed); }
 
 void Sequence::Set(const long& value) {
     this->value_.store(value, memory_order_relaxed);
@@ -25,16 +21,14 @@ bool Sequence::CompareAndSet(long& expect, const long& newValue) {
     return value_.compare_exchange_weak(expect, newValue, memory_order_seq_cst);
 }
 
-long Sequence::IncrementAndGet() {
-    return AddAndGet(1);
-}
+long Sequence::IncrementAndGet() { return AddAndGet(1); }
 
 long Sequence::AddAndGet(const long& increment) {
     long currentValue, newValue;
     do {
         currentValue = Get();
         newValue = currentValue + increment;
-    } while(!CompareAndSet(currentValue, newValue));
+    } while (!CompareAndSet(currentValue, newValue));
     return newValue;
 }
 
--- src/Sequencer.cc
+++ src/Sequencer.cc
@@ -4,33 +4,24 @@ using namespace qqibrow::Disruptor;
 using namespace boost;
 using namespace std;
 
-
-Sequencer::Sequencer(int bufferSize, WaitStrategy* strategy) :
-    bufferSize_(bufferSize),
-    waitStrategy_(strategy)
-{
-    assert(bufferSize > 1 && ((bufferSize & (bufferSize - 1)) == 0) && "buffSize is wrong.");
+Sequencer::Sequencer(int bufferSize, WaitStrategy* strategy)
+    : bufferSize_(bufferSize), waitStrategy_(strategy) {
+    assert(bufferSize > 1 && ((bufferSize & (bufferSize - 1)) == 0) &&
+           "buffSize is wrong.");
 }
 
-long Sequencer::GetCursor() const {
-    return cursor_.Get();
-}
+long Sequencer::GetCursor() const { return cursor_.Get(); }
 
-int Sequencer::GetBufferSize() const {
-    return bufferSize_;
-}
+int Sequencer::GetBufferSize() const { return bufferSize_; }
 
 void Sequencer::AddGatingSequence(Sequence* sequence) {
     gatingSequences_.push_back(SequencePtr(sequence));
 }
 void Sequencer::RemoveGatingSequence(Sequence* sequence) {
-    remove_if(gatingSequences_.begin(), gatingSequences_.end(),
-            [&sequence](const SequencePtr& ptr) { return ptr.get() == sequence;});
+    remove_if(
+        gatingSequences_.begin(), gatingSequences_.end(),
+        [&sequence](const SequencePtr& ptr) { return ptr.get() == sequence; });
 }
-long Sequencer::GetMinimumSequence() const {
-    return 0;
-}
-SequenceBarrier Sequencer::NewBarrier(Sequence sequenceToTrack) {
-    return 0;
-}
+long Sequencer::GetMinimumSequence() const { return 0; }
+SequenceBarrier Sequencer::NewBarrier(Sequence sequenceToTrack) { return 0; }
 
