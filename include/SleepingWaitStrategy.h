6092b94bf8e5620de5262b581922ea48ab136235
qqibrow <qqibrow@gmail.com>
2014-10-04 14:09:46 -0700

    Finish basic structure of SleepingWaitStratety.

--- include/SleepingWaitStrategy.h
+++ include/SleepingWaitStrategy.h
@@ -1,13 +1,20 @@
 #ifndef QQIBROW_DISRUPTOR_SLEEPINGWAITSTRATEGY
 #define QQIBROW_DISRUPTOR_SLEEPINGWAITSTRATEGY
 #include "WaitStrategy.h"
+#include <boost/scoped_ptr.hpp>
 
 namespace qqibrow {
 namespace Disruptor {
 class SleepingWaitStrategy : public WaitStrategy {
-    long WaitFor(long sequence, const Sequence& cursor,
-                 const Sequence& dependentSequence, SequenceBarrier& barrier);
-    void SignalAllWhenBlocking();
+   public:
+    SleepingWaitStrategy();
+    virtual long WaitFor(long sequence, const Sequence& cursor,
+                         const Sequence& dependentSequence,
+                         const SequenceBarrier& barrier);
+    virtual void SignalAllWhenBlocking();
+   private:
+    class ConcurrencyImpl;
+    boost::scoped_ptr<ConcurrencyImpl> concurrencyUtil_;
 };
 
 }  // namespace Disruptor
--- src/SleepingWaitStrategy.cc
+++ src/SleepingWaitStrategy.cc
@@ -1,11 +1,37 @@
 #include "SleepingWaitStrategy.h"
+#include <mutex>
+#include <condition_variable>
 
 using namespace qqibrow::Disruptor;
+using namespace std;
+
+class SleepingWaitStrategy::ConcurrencyImpl {
+   public:
+    mutex mtx;
+    condition_variable cv;
+};
+
+SleepingWaitStrategy::SleepingWaitStrategy()
+    : concurrencyUtil_(new ConcurrencyImpl()) {}
 
 long SleepingWaitStrategy::WaitFor(long sequence, const Sequence& cursor,
                                    const Sequence& dependentSequence,
-                                   const SequenceBarrier& barrier) {}
+                                   const SequenceBarrier& barrier) {
+    long availableSequence;
+    if ((availableSequence = cursor.Get()) < sequence) {
+        unique_lock<mutex> lk(concurrencyUtil_->mtx);
+        while ((availableSequence = cursor.Get()) < sequence) {
+            // barrier.CheckAlert();
+            concurrencyUtil_->cv.wait(lk);
+        }
+    }
+    while ((availableSequence = dependentSequence.Get()) < sequence) {
+        // barrier.CheckAlert();
+    }
+    return availableSequence;
+}
 
 void SleepingWaitStrategy::SignalAllWhenBlocking() {
-
+    unique_lock<mutex> lk(concurrencyUtil_->mtx);
+    concurrencyUtil_->cv.notify_all();
 }
