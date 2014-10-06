ba25aeb9e2f7c54fa1f46951bdd71b5100225dca
qqibrow <qqibrow@gmail.com>
2014-10-05 22:37:35 -0700

    Add Util.h

---
+++ include/Util.h
@@ -0,0 +1,25 @@
+#ifndef QQIBROW_DISRUPTOR_UTIL
+#define QQIBROW_DISRUPTOR_UTIL
+#include "Sequence.h"
+#include <algorithm>
+#include <vector>
+
+namespace qqibrow {
+namespace Disruptor {
+struct CompareSequencePtr {
+    bool operator()(const SequencePtr& p1, const SequencePtr& p2) {
+        return p1->Get() < p2->Get();
+    }
+};
+class Util {
+   public:
+    static long GetMinSequence(const std::vector<SequencePtr>& gatingSequences,
+                               long sequence) {
+        const SequencePtr& ptr = *std::min_element(gatingSequences.begin(), gatingSequences.end(),
+                             CompareSequencePtr());
+        return (std::min)(sequence,ptr->Get());
+    }
+};
+}
+}
+#endif
